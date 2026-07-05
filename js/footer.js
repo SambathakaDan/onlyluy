0aeeb99eae97dbe3bcec657cf78315937f7ca841
SambathakaDan <sambathonthework@gmail.com>
2026-07-05 19:58:22 +0700

    Added global footer to all pages, still need fixes around T&C and alignment issue

--- js/footer.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const copyElement = document.querySelector('.footer-copy');
-    if (copyElement) {
-        const currentYear = new Date().getFullYear();
-        copyElement.textContent = copyElement.textContent.replace('2026', currentYear);
-    }
-});
